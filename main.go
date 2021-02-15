6d49d725e69df8b1a8fedf6f65eda859b12ecd23
mikalep <itstenlayers@gmail.com>
2021-02-15 09:03:24 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// List all available options for the user
+func listOptions {
+}
+
+// Functions to handle each specific task
+func showAllTasks {
+}
+
+func addNewTask {
+}
+
+func EditTask {
+}
+
+func deleteTask {
+}
+
+func StatusOfTask {
+}
+
+// Handle the actual user choice
+func chooseAction {
+	// switch case?
+}
+
+func main() {
+	fmt.Println("Hey there friend, welcome to To-Do Commander.")
+}
